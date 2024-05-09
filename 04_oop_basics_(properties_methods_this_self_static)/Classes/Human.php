bd496306ef490d9925234c2426b7084cdca58029
Mohamed Mostafa <mokammel0000@gmail.com>
2024-05-09 08:00:43 +0300

    oop basics in php
    properties - methods - this - self - static

---
+++ 04_oop_basics_(properties_methods_this_self_static)/Classes/Human.php
@@ -0,0 +1,29 @@
+<?php
+
+class Human
+{
+    const NUMBER_OF_EYES = 2;
+
+    public $name;
+    public $age;
+    public $height;
+
+    public function setDetails($name, $age, $height)
+    {
+        $this->name = $name;
+        $this->age = $age;
+        $this->height = $height;
+    }
+
+    public function getDetails()
+    {
+        echo  '-------------------the get details method:-----------------------<br>';
+        echo  Self::NUMBER_OF_EYES . '<br>';
+        echo  static::NUMBER_OF_EYES . '<br>';
+
+        echo 'this is a human <br>';
+        echo "his name is {$this->name} <br>";
+        echo "his age is {$this->age} <br>";
+        echo "his height is {$this->height} <br>";
+    }
+}
