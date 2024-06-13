7ed9e0d579c330deb29ba6d64626c0fae72fe845
Mohamed Mostafa <mokammel0000@gmail.com>
2024-06-13 17:25:41 +0300

    variable scope

--- 05_variables/index.php
+++ 05_variables/index.php
@@ -4,8 +4,8 @@
  * variable stores a value of any type (string, number, array, object).
  *
  * variable naming rules:
- *      - The variable name must start with the dollar sign ($).
- *      - The first character after the dollar sign ( $) must be a letter (a-z) or the underscore ( _ ).
+ *      - must start with the dollar sign ($).
+ *      - first character after the dollar sign ( $) must be a letter (a-z) or the underscore (_).
  *      - The remaining characters can be underscores, letters, or numbers.
  *
  * PHP variables are case-sensitive ($message != $Message).
---
+++ 06_variable_scope/home.php
@@ -0,0 +1,4 @@
+<?php
+
+echo NAME;
+echo $x;
