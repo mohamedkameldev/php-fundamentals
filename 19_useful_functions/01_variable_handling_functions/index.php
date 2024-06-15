245eb751c2d2135f7f9e992af715dbbb32c380e9
Mohamed Mostafa <mokammel0000@gmail.com>
2024-06-15 22:01:09 +0300

    variable handling functions

---
+++ 19_useful_functions/01_variable_handling_functions/index.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * boolval()
+ * doubleval()
+ * floatval()
+ * intval()
+ * strval()
+ * debug_zval_dump()
+ *
+ * is_countable()
+ * is_array()
+ * is_bool()
+ * is_callable()
+ * is_double()
+ * is_float()
+ * is_int()
+ * is_integer()
+ * is_iterable()
+ * is long()
+ * is_float()
+ * is_int()
+ * is_integer()
+ * is_iterable()
+ * is_long0
+ * is_null()
+ * is_numeric()
+ * is_object()
+ * is_real()
+ * is_resource()
+ * is_scalar()
+ * is_string()
+ * isset()
+ *
+ * var_dump()
+ * gettype()
+ * settype()
+ * empty()
+ * print_r()
+ * unset()
+ *
+ * get_defined_vars()
+ * get_resource_type()
+ *
+ * serialize()
+ * unserialize()
+ * var_export()
+ */
