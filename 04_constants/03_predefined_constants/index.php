6cc71fdfb8e1ec3774fa913c6c16c0c296baf977
Mohamed Mostafa <mokammel0000@gmail.com>
2024-06-16 23:28:04 +0300

    predefined constants

---
+++ 04_constants/03_predefined_constants/index.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Pre-Defined Constants [Case Sensitive]
+ *
+ * search for: PHP Predefined Constants (عددهم كبير جدا)
+ */
+
+echo php_uname() . '<br>';
+
+echo PHP_OS_FAMILY . '<br>';
+
+echo PHP_VERSION . '<br>';
+
+echo DEFAULT_INCLUDE_PATH . '<br>';
+
+echo PHP_INT_MAX . '<br>';
