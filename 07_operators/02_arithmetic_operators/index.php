b9c6b8f9535c5a82c1312f371ddbe563fac98805
Mohamed Mostafa <mokammel0000@gmail.com>
2024-06-17 18:51:18 +0300

    arithmetic operators

---
+++ 07_operators/02_arithmetic_operators/index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+/*
+ * Arithmetic Operators:
+ *      -used to perform common arithmetic operations.
+ *      -require numeric values, If you apply them to non-numeric values,
+ *      they’ll convert them to numeric values before carrying the arithmetic operation.
+ *
+ *  +  (Addition).
+ *  -  (Subtraction.
+ *  *  (Multiplication).
+ *  /  (Division).
+ *  %  (Modulus).
+ *  ** (Exponentiation).
+ */
