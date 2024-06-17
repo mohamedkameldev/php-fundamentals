e4704e3560d2866a9c952d310efdc514bd9865e9
Mohamed Mostafa <mokammel0000@gmail.com>
2024-06-17 18:54:08 +0300

    string operators

---
+++ 07_operators/09_string_operators/index.php
@@ -0,0 +1,10 @@
+<?php
+
+
+/*
+ * String Operators: used to manipulate strings.
+ *
+ * . (Concatenation): Concatenates two strings.
+ * .= (Concatenation assignment):
+ *              Appends the right operand to the left operand and assigns the result to the left operand.
+ */
