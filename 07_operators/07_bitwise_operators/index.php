71a07ecb127776c9e5215829da513ffcdba0c1bb
Mohamed Mostafa <mokammel0000@gmail.com>
2024-06-17 18:52:31 +0300

    bitwise operators

---
+++ 07_operators/07_bitwise_operators/index.php
@@ -0,0 +1,15 @@
+<?php
+
+
+/*
+ * Bitwise Operators:
+ *      used to perform bit-level operations.
+ *      perform operations on the binary representation of the operands.
+ *
+ * & (And).
+ * | (Or).
+ * ^ (Xor).
+ * ~ (Not).
+ * << (Shift left).
+ * >> (Shift right).
+ */
