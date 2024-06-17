ba887578cefa6cd74430a8144fa244da00702eab
Mohamed Mostafa <mokammel0000@gmail.com>
2024-06-17 18:52:48 +0300

    ternary operator

---
+++ 07_operators/08_conditional_assignment_operators/01_ternary_operator/index.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * Ternary Operator: a shorthand for the if...else statement.
+ *
+ *      condition ? value the true case : value the false case;
+ */
+
+$is_user_logged_in = false;
+
+echo $is_user_logged_in ? 'welcome' : 'please login again' . '<br>';
