542b5bd5e1f8eb41085753009186e7bb8eb46e68
Mohamed Mostafa <mokammel0000@gmail.com>
2024-06-18 23:07:36 +0300

    do while alternative syntax

---
+++ 10_loops/07_do_while_alternative_syntax/index.php
@@ -0,0 +1,9 @@
+<?php
+
+
+/**
+ * PHP does not support an alternative syntax for the do...while loop,
+ * like it does for if, while, for, and foreach.
+ *
+ * do...while loop must use the standard curly brace {} syntax.
+ */
