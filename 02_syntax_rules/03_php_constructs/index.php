de892b4bcf3e6329b8da4919e6503301fa70c844
Mohamed Mostafa <mokammel0000@gmail.com>
2024-06-18 12:29:40 +0300

    if else

--- 02_syntax_rules/03_php_constructs/index.php
+++ 02_syntax_rules/03_php_constructs/index.php
@@ -6,6 +6,8 @@
  * Constructs are fundamental parts of PHP's grammar,
  * and are used to define control structures, output data, handle variables, and more.
  *
+ * Some constructs do not require parentheses when used, such as echo or exit.
+ *
  * Constructs are integral to the PHP syntax and are parsed directly by the PHP interpreter.
  *
  * Constructs are generally more efficient in terms of execution compared to functions or methods,
