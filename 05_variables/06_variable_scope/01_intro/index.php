20744a87244c5c49d57aa66afb981caf027edcf6
Mohamed Mostafa <mokammel0000@gmail.com>
2024-06-19 03:47:22 +0300

    local variable

---
+++ 05_variables/06_variable_scope/01_intro/index.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * variable in PHP can have 3 scopes:
+ * 1- local variable => local between {}
+ *
+ * 2- global variable => global through any line in the same page.
+ *
+ * 3- superglobal variable => global between the whole project (among the pages).
+ */
