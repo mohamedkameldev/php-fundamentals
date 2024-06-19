5221781f36f2eb55ceb30666c0abaf8a0e2c941e
Mohamed Mostafa <mokammel0000@gmail.com>
2024-06-19 15:06:28 +0300

    php reserved words

---
+++ 02_syntax_rules/03_php_reserved_words/index.php
@@ -0,0 +1,62 @@
+<?php
+
+
+/**
+ * Reserved words:
+ * -words that are reserved by the language for a specific purpose.
+ * -words that have special meanings defined by the language.
+ * -words that integral to PHP syntax and cannot be used as,
+ *      identifiers for variables, functions, classes, or any other user-defined symbols.
+ * -examples:
+ *
+ * 01-Control Structures:
+ *      if, else, elseif, endif
+ *      switch, case, default
+ *      while, do, for, foreach, endwhile, endfor, endforeach
+ *      break, continue
+ *
+ * 02-Class and Object Keywords:
+ *      class, abstract, interface, trait
+ *      extends, implements
+ *      public, protected, private
+ *      const, static, final
+ *      new, clone
+ *
+ * 03-Function Keywords:
+ *      function, return
+ *      yield, yield from
+ *
+ * 04-Include and Require:
+ *      include, include_once, require, require_once
+ *
+ * 05-Namespace and Importing:
+ *      namespace, use, as
+ *
+ * 06-Type Declarations:
+ *      int, float, bool, string, array, object, callable, iterable
+ *      void, mixed, never
+ *
+ * 07-Exception Handling:
+ *      try, catch, finally, throw
+ *      Throwable, Error, Exception, finally
+ *
+ * 08-Miscellaneous:
+ *      echo, print
+ *      global, static
+ *      isset, unset, empty
+ *      eval
+ *      exit, die
+ *      list
+ *      array
+ *      declare
+ *      goto
+ *
+ * 09-Type Declarations:
+ *      self, parent
+ *
+ * 10-Null Coalescing:
+ *      ??
+ *
+ * 11-Generator Function:
+ *      yield
+ */
